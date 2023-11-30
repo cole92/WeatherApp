84ea9afabbc4fb297a412d3c334d9385f1bc3d9e
cole92 <aleksandar9212@gmail.com>
2023-11-30 19:03:02 +0100

    Added 'getResults' and 'displayResults' functions.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const highLow = document.querySelector('.hi-low');
 // Creating an API
 const api = {
     key: "3045dd712ffe6e702e3245525ac7fa38",
-    base: "https://api.openweathermap.org/data2.5/"
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 //Accessing the input field
@@ -20,3 +20,19 @@ searchBox.addEventListener('keypress', event => {
         getResults(searchBox.value);
     }
 })
+
+const getResults = query => {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => weather.json())
+    .then(displayResults);
+    console.log(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
+}
+
+const displayResults = weather => {
+    city.innerText = `${weather.name}, ${weather.sys.contry}`;
+    let now = new Date();
+    date.innerText = dateBuilder(now);
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+    weather.innerText = weather.weather[0].main;
+    highLow.innerText = `${Math.round(weather.main.temp.temp_min)}°c / ${Math.round(weather.main.temp.temp_max)}°c`;
+}
